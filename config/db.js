894c45ae668b2df5dee533e7d7f17e3bb91619df
TruptiWalunjkar <truptiwalunjkar@gmail.com>
2025-04-23 17:16:51 +0530

    database

--- config/db.js
+++ config/db.js
@@ -1,26 +1,28 @@
 import mongoose from "mongoose";
-import dotenv from "dotenv";
-
-dotenv.config();
 
 let cached = global.mongoose;
 
 if (!cached) {
     cached = global.mongoose = { conn: null, promise: null };
 }
+
 async function connectDB() {
     if (cached.conn) {
         return cached.conn;
     }
+
     if (!cached.promise) {
         const opts = {
             bufferCommands: false,
-        }
-        cached.promise = mongoose.connect('${process.env.MONGODB_URI}/gearup',opts).then((mongoose) => {
+        };
+
+        cached.promise = mongoose.connect(`${process.env.MONGODB_URI}/gearup`, opts).then((mongoose) => {
             return mongoose;
         });
     }
+
     cached.conn = await cached.promise;
     return cached.conn;
 }
+
 export default connectDB;
